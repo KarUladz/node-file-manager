27bad098900780ac762ddb217bb8a504c5d17afa
Kornull <u.karnialiuk@gmail.com>
2025-05-04 14:06:05 +0300

    fix: up

--- src/services/fs/operationUp.js
+++ src/services/fs/operationUp.js
@@ -3,9 +3,15 @@ import path from "node:path";
 import currentPath from "../../utils/current-path.js";
 
 export const operationUp = async () => {
-  const pathNow = currentPath.getPath();
+  const pathParsed = path.parse(currentPath.getPath());
+  const rootDir = pathParsed.root;
 
-  currentPath.setPath(path.dirname(pathNow));
+  if (rootDir === currentPath.getPath()) {
+    console.log("Invalid input");
+  } else {
+    const pathNow = currentPath.getPath();
+    currentPath.setPath(path.dirname(pathNow));
+  }
 
   return;
 };
