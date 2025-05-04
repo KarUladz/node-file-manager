fd11723f4d998265ea57251883c8d4f1e7bbe8ca
Kornull <u.karnialiuk@gmail.com>
2025-05-04 13:49:22 +0300

    fix: createEmptyFile

--- src/services/fs/createEmptyFile.js
+++ src/services/fs/createEmptyFile.js
@@ -1,11 +1,20 @@
 import fs from "node:fs";
+import path from "node:path";
+import currentPath from "../../utils/current-path.js";
 
 import { normalizePathString } from "../../utils/normalizePathString.js";
 
 export const createEmptyFile = async (comandKey, data) => {
   const normalizePath = normalizePathString(comandKey, data);
-  console.log(normalizePath);
-  fs.open(normalizePath, "ax", (err, _) => {
-    if (err) console.log("Operation failed!\n");
-  });
+  const pathNow = path.normalize(
+    `${currentPath.getPath()}/${path.basename(normalizePath)}`
+  );
+
+  if (pathNow === normalizePath) {
+    fs.open(normalizePath, "ax", (err, _) => {
+      if (err) console.log("Operation failed! File already exists\n");
+    });
+  } else {
+    console.log("Invalid input");
+  }
 };
